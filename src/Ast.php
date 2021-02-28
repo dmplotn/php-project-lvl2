a14b1bfd9932f209b14e268b82723f648e67e1e5
Dmitry Plotnikov <dmplotn@gmail.com>
2021-02-28 23:40:50 +0500

    refactor

--- src/Ast.php
+++ src/Ast.php
@@ -10,36 +10,15 @@ function buildAst($data1, $data2)
     sort($keys);
     return array_map(function ($key) use ($data1, $data2) {
         if (!array_key_exists($key, $data1)) {
-            $node = [
-                'name' => $key,
-                'type' => 'added',
-                'afterValue' => $data2[$key]
-            ];
+            $node = ['name' => $key, 'type' => 'added', 'afterValue' => $data2[$key]];
         } elseif (!array_key_exists($key, $data2)) {
-            $node = [
-                'name' => $key,
-                'type' => 'deleted',
-                'beforeValue' => $data1[$key]
-            ];
+            $node = ['name' => $key, 'type' => 'deleted', 'beforeValue' => $data1[$key]];
         } elseif (is_array($data1[$key]) && is_array($data2[$key])) {
-            $node = [
-                'name' => $key,
-                'type' => 'nested',
-                'children' => buildAst($data1[$key], $data2[$key])
-            ];
+            $node = ['name' => $key, 'type' => 'nested', 'children' => buildAst($data1[$key], $data2[$key])];
         } elseif ($data1[$key] !== $data2[$key]) {
-            $node = [
-                'name' => $key,
-                'type' => 'changed',
-                'beforeValue' => $data1[$key],
-                'afterValue' => $data2[$key]
-            ];
+            $node = ['name' => $key, 'type' => 'changed', 'beforeValue' => $data1[$key], 'afterValue' => $data2[$key]];
         } else {
-            $node = [
-                'name' => $key,
-                'type' => 'unchanged',
-                'beforeValue' => $data1[$key]
-            ];
+            $node = ['name' => $key, 'type' => 'unchanged', 'beforeValue' => $data1[$key]];
         }
 
         return $node;
