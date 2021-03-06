d3344aa6b341cbf0736702aef610d8ff594178a3
Dmitry Plotnikov <dmplotn@gmail.com>
2021-03-06 14:54:22 +0500

    add plain formater

--- src/Formaters.php
+++ src/Formaters.php
@@ -2,12 +2,14 @@
 
 namespace Diff\Generator\Formaters;
 
-use function Diff\Generator\Formatting\Formaters\StylishFormater\formatAst;
+use function Diff\Generator\Formatting\Formaters\StylishFormater\formatAst as stylishFormatAst;
+use function Diff\Generator\Formatting\Formaters\PlainFormater\formatAst as plainFormatAst;
 
 function getFormater($format)
 {
     $mapping = [
-        'stylish' => fn($ast) => formatAst($ast)
+        'stylish' => fn($ast) => stylishFormatAst($ast),
+        'plain' => fn($ast) => plainFormatAst($ast)
     ];
 
     return $mapping[$format] ?? $mapping['stylish'];
---
+++ src/Formaters/PlainFormater.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Diff\Generator\Formatting\Formaters\PlainFormater;
+
+use function Funct\Collection\flatten;
+
+function formatAst($ast, $nameParts = [])
+{
+    $parts = array_map(function ($node) use ($nameParts) {
+        $name = $node['name'];
+        $type = $node['type'];
+        $beforeValue = valueAsStr($node['beforeValue'] ?? null);
+        $afterValue = valueAsStr($node['afterValue'] ?? null);
+        $children = $node['children'] ?? null;
+        $fullName = implode('.', [...$nameParts, $name]);
+
+        switch ($type) {
+            case 'added':
+                $part = "Property '{$fullName}' was added with value: {$afterValue}";
+                break;
+            case 'deleted':
+                $part = "Property '{$fullName}' was removed";
+                break;
+            case 'nested':
+                $part = formatAst($children, [...$nameParts, $name]);
+                break;
+            case 'changed':
+                $part = "Property '{$fullName}' was updated. From {$beforeValue} to {$afterValue}";
+                break;
+            case 'unchanged':
+                $part = [];
+                break;
+            default:
+                throw new \LogicException("Unknown node type {$type}");
+        }
+
+        return $part;
+    }, $ast);
+
+    return implode("\n", flatten($parts));
+}
+
+function valueAsStr($value)
+{
+    if (is_bool($value)) {
+        return $value ? 'true' : 'false';
+    }
+
+    if (is_null($value)) {
+        return 'null';
+    }
+
+    if (is_string($value)) {
+        return "'{$value}'";
+    }
+
+    return is_array($value) ? '[complex value]' : (string) $value;
+}
